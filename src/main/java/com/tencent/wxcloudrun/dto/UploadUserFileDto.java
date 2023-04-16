80c8cdcdc9226e5abbae6fe98d1150e2c1955412
wangguangyu0303 <874738684@qq.com>
2023-04-16 12:50:48 +0800

    对openid加密

--- src/main/java/com/tencent/wxcloudrun/dto/UploadUserFileDto.java
+++ src/main/java/com/tencent/wxcloudrun/dto/UploadUserFileDto.java
@@ -1,12 +1,14 @@
 package com.tencent.wxcloudrun.dto;
 
 import lombok.Data;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 @Slf4j
 @Data
+@ToString
 public class UploadUserFileDto {
 
 
